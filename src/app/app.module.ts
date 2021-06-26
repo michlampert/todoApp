9e94b68d758011385283b928eace45213e1b70ec
michlampert <michlampert@gmail.com>
2021-06-26 19:10:37 +0200

    create basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AddTaskComponent } from './add-task/add-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    AddTaskComponent
   ],
   imports: [
     BrowserModule
